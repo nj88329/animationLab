81c45ee8f44363632238767ebafd943e810e6465
nitin joshi <nj88329@gmail.com>
2025-08-30 18:49:11 +0530

    Framer and Gsap code changed

--- src/components/Shoutout.tsx
+++ src/components/Shoutout.tsx
@@ -5,9 +5,21 @@ const cardVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.8, staggerChildren: 0.3 }
+    transition: { duration: 0.5, staggerChildren: 0.3 }
   }
 };
+
+// Parent controls staggering
+const parentVariants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.5, // delay before next grid starts
+      repeat: Infinity, // loop forever,
+     zIndex : 2
+    }
+  }
+};
+
 
 const childVariants = {
   hidden: { opacity: 0, y: 50 },
@@ -17,22 +29,36 @@ const childVariants = {
 const Shoutout = () => {
   return (
     <div className = 'bg-gray-900'>
-    <motion.div className="outergrid ml-[10vw] w-[80vw] h-[80vh] rounded-xl shadow-xl shadow-orange-900 bg-gradient-to-l from-purple-900 to-black grid grid-cols-3 items-center">
+    <motion.div 
+       variants={parentVariants}
+        initial="hidden"
+        animate="animate" 
+    className="outergrid ml-[10vw] w-[80vw] h-[80vh] rounded-xl shadow-xl shadow-orange-900 bg-gradient-to-l from-purple-900 to-black grid grid-cols-3 items-center">
       
       {/* Left grid */}
-      <motion.div className="leftgrid h-[50vh] w-[25vw]  ml-3 rounded-xl flex items-center justify-center  bg-gradient-to-r bg-orange-900 to-black ">
+      <motion.div initial={{ opacity: 0 , y : 40 }}
+        animate={{ opacity: [1, 0, 1] , backgroundImage: [
+      "linear-gradient(to right, #991b1b, #000000)", 
+            "linear-gradient(to right, #065f46, #eab308)", 
+    ] , y : 0 }}
+        transition={{
+          duration: 3,
+          ease: "easeInOut",
+          repeat: Infinity
+        }} 
+       className="leftgrid h-[50vh] w-[25vw]  ml-3 rounded-xl flex items-center justify-center  bg-gradient-to-r bg-orange-900 to-black ">
         left
       </motion.div>
 
       {/* Mid grid with cards stacked */}
       <motion.div
-        variants={cardVariants}
+        variants={cardVariants} 
         initial="hidden"
         whileInView="visible"
         className="midgrid bg-gradient-to-r bg-orange-900 to-black h-[50vh] w-[25vw] rounded-xl relative flex  "
       >
         {/* Right card */}
-        <motion.div
+        <motion.div  
           variants={childVariants}
           className="absolute card right bg-gradient-to-b from-green-800 to-black h-[25vh] w-[20vw] ml-15 mt-10 flex items-center justify-center rounded-xl"
           style={{ transform: "translateX(40px) rotate(10deg)" }}
@@ -43,7 +69,7 @@ const Shoutout = () => {
         </motion.div>
 
         {/* Middle card */}
-        <motion.div
+        <motion.div 
           variants={childVariants}
           className="absolute card mid bg-gradient-to-t from-green-800 to-black h-[25vh] w-[20vw] ml-10 mt-7 flex items-center justify-center rounded-xl"
         >
@@ -65,9 +91,35 @@ const Shoutout = () => {
       </motion.div>
 
       {/* Right grid */}
-      <motion.div className="rightgrid h-[50vh] w-[25vw]  bg-gradient-to-r bg-orange-900 to-black  -ml-4 rounded-xl flex items-center justify-center">
+      <motion.div   initial={{ opacity: 0 , y : 0 }}
+        animate={{ opacity: [0, 1, 0 , 1] , y : 40,  backgroundImage: [
+      "linear-gradient(to right, #7c2d12, #000000)", // orange → black
+      "linear-gradient(to right, #065f46, #000000)", // green → black
+    ] }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity
+        }} 
+  className="rightgrid h-[50vh] w-[25vw]  bg-gradient-to-r bg-orange-900 to-black  -ml-4 rounded-xl flex items-center justify-center">
         right
       </motion.div>
+
+      <motion.div  initial={{  x : 200 , backgroundColor :"#d1d5db" }}
+      animate = {{x:700 , backgroundColor : "#000000" }}
+       transition={{
+          duration: 2,
+          ease: "easeInOut",
+           repeatType: "reverse",
+          repeat: Infinity
+        }} 
+       className = ' h-[20vh] w-[20vw] flex items-center justify-center shadow-lg shadow-red-900'>
+            <h2 className = 'font-bold text-white shadow-red-500 shadow-lg'>
+               He does a great work.
+            </h2>
+      </motion.div>
+       
+
     </motion.div>
     </div>
   );
