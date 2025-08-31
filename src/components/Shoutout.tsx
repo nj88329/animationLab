6da2ac406c58d4c07702fcfe139c64cb9332d586
nitin joshi <nj88329@gmail.com>
2025-08-31 12:57:03 +0530

    new changes made for the shoutout components and  made it resposive

--- src/components/Shoutout.tsx
+++ src/components/Shoutout.tsx
@@ -29,11 +29,11 @@ const childVariants = {
 const Shoutout = () => {
   return (
     <div className = 'bg-gray-900'>
-    <motion.div 
+      <motion.div 
        variants={parentVariants}
         initial="hidden"
         animate="animate" 
-    className="outergrid ml-[10vw] w-[80vw] h-[80vh] rounded-xl shadow-xl shadow-orange-900 bg-gradient-to-l from-purple-900 to-black grid grid-cols-3 items-center">
+       className="outergrid ml-[10vw]  lg:w-[80vw] lg:h-[80vh] rounded-xl shadow-xl shadow-orange-900 bg-gradient-to-l from-purple-900 to-black grid grid-rows-3 md:grid-cols-3 place-items-center ">
       
       {/* Left grid */}
       <motion.div initial={{ opacity: 0 , y : 40 }}
@@ -46,7 +46,7 @@ const Shoutout = () => {
           ease: "easeInOut",
           repeat: Infinity
         }} 
-       className="leftgrid h-[50vh] w-[25vw]  ml-3 rounded-xl flex items-center justify-center  bg-gradient-to-r bg-orange-900 to-black ">
+       className="leftgrid h-[20vh] w-[80%]   md:h-[50vh] md:w-[25vw]  md:ml-3 rounded-xl flex items-center justify-center  bg-gradient-to-r bg-orange-900 to-black ">
         left
       </motion.div>
 
@@ -55,15 +55,15 @@ const Shoutout = () => {
         variants={cardVariants} 
         initial="hidden"
         whileInView="visible"
-        className="midgrid bg-gradient-to-r bg-orange-900 to-black h-[50vh] w-[25vw] rounded-xl relative flex  "
+        className="midgrid bg-gradient-to-r bg-orange-900 to-black h-[20vh]  w-[80%]  md:h-[50vh] md:w-[25vw] rounded-xl relative flex" 
       >
         {/* Right card */}
         <motion.div  
           variants={childVariants}
-          className="absolute card right bg-gradient-to-b from-green-800 to-black h-[25vh] w-[20vw] ml-15 mt-10 flex items-center justify-center rounded-xl"
+          className="absolute card right w-[14vh]  h-[70%] w-[30%] md:h-[25vh] md:w-[20vw]  bg-gradient-to-b from-green-800 to-black  ml-15 mt-10 flex items-center justify-center rounded-xl overflow-hidden"
           style={{ transform: "translateX(40px) rotate(10deg)" }}
         >
-          <h1 className="heading text-yellow-600 font-bold text-center">
+          <h1 className="heading text-yellow-600  text-sm  md:font-bold text-center">
             TAKE YOUR PRODUCT FROM ZERO<br />right
           </h1>
         </motion.div>
@@ -71,9 +71,9 @@ const Shoutout = () => {
         {/* Middle card */}
         <motion.div 
           variants={childVariants}
-          className="absolute card mid bg-gradient-to-t from-green-800 to-black h-[25vh] w-[20vw] ml-10 mt-7 flex items-center justify-center rounded-xl"
+          className="absolute card mid w-[14vh] bg-gradient-to-t from-green-800 to-black h-[70%] w-[30%] md:h-[25vh] md:w-[20vw] ml-10 mt-7 flex items-center justify-center rounded-xl overflow-hidden"
         >
-          <h1 className="heading text-yellow-600 font-bold text-center">
+          <h1 className="heading text-yellow-600  text-sm  md:font-bold text-center">
             TAKE YOUR PRODUCT FROM ZERO<br />left
           </h1>
         </motion.div>
@@ -81,10 +81,10 @@ const Shoutout = () => {
         {/* Left card */}
         <motion.div
           variants={childVariants}
-          className="absolute card left bg-gradient-to-t from-green-800 to-black h-[25vh] w-[20vw] flex items-center justify-center rounded-xl"
+          className="absolute card left  h-[70%] w-[30%] md:h-[25vh] md:w-[20vw]  bg-gradient-to-t from-green-800 to-black h-[25vh] w-[20vw] flex items-center justify-center rounded-xl overflow-hidden"
           style={{ transform: "translateX(-40px) rotate(-10deg)" }}
         >
-          <h1 className="heading text-yellow-600 font-bold text-center">
+          <h1 className="heading text-yellow-600 text-sm md:font-bold text-center ">
             TAKE YOUR PRODUCT FROM ZERO<br />mid
           </h1>
         </motion.div>
@@ -101,25 +101,57 @@ const Shoutout = () => {
           ease: "easeInOut",
           repeat: Infinity
         }} 
-  className="rightgrid h-[50vh] w-[25vw]  bg-gradient-to-r bg-orange-900 to-black  -ml-4 rounded-xl flex items-center justify-center">
+  className="rightgrid  h-[20vh]   w-[80%]  md:h-[50vh] md:w-[25vw] bg-gradient-to-r bg-orange-900 to-black  md:-ml-4 rounded-xl flex items-center justify-center">
         right
       </motion.div>
 
-      <motion.div  initial={{  x : 200 , backgroundColor :"#d1d5db" }}
-      animate = {{x:700 , backgroundColor : "#000000" }}
+   <div className = 'flex items-center justify-center gap-4 w-[80vw]'>
+      <motion.div  initial={{  x : 50 , backgroundColor :"#0d7558ff" }}
+        animate = {{x:150 , backgroundColor : "#1a68d6ff" }}
        transition={{
           duration: 2,
           ease: "easeInOut",
            repeatType: "reverse",
           repeat: Infinity
         }} 
-       className = ' h-[20vh] w-[20vw] flex items-center justify-center shadow-lg shadow-red-900'>
+       className = ' h-[20vh] w-[20vw] lg:h-[40vh] flex items-center justify-center shadow-lg rounded-lg shadow-red-900 [clip-path:polygon(0_10%,85%_10%,93%_0%,100%_5%,100%_100%,0_100%)]'>
             <h2 className = 'font-bold text-white shadow-red-500 shadow-lg'>
-               He does a great work.
+                He does a great work.<br/>
+               Does his work with <br/>
+                great perfection.
             </h2>
       </motion.div>
-       
-
+        <motion.div  initial={{  x : 50 , backgroundColor :"#065f46" }}
+        animate = {{x:150 , backgroundColor : "#85a40aff" }}
+       transition={{
+          duration: 2,
+          ease: "easeInOut",
+           repeatType: "reverse",
+          repeat: Infinity
+        }} 
+       className = ' h-[20vh] w-[20vw]  lg:h-[40vh] flex items-center justify-center shadow-lg rounded-lg shadow-red-900  [clip-path:polygon(0_10%,85%_10%,93%_0%,100%_5%,100%_100%,0_100%)]'>
+            <h2 className = 'font-bold text-white shadow-red-500 shadow-lg'>
+                 He does a great work.<br/>
+               Does his work with <br/>
+                great perfection.
+            </h2>
+      </motion.div>
+       <motion.div  initial={{  x : 50, backgroundColor :"#0d695aff" }}
+        animate = {{x:150 , backgroundColor : "#6e0b0bff" }}
+       transition={{
+          duration: 2,
+          ease: "easeInOut",
+           repeatType: "reverse",
+          repeat: Infinity
+        }} 
+       className = ' h-[20vh] w-[20vw]  lg:h-[40vh] flex items-center justify-center rounded-lg shadow-lg shadow-red-900  [clip-path:polygon(0_10%,85%_10%,93%_0%,100%_5%,100%_100%,0_100%)]'>
+            <h2 className = 'font-bold text-white shadow-red-500 shadow-lg'>
+                 He does a great work.<br/>
+               Does his work with <br/>
+                great perfection.
+            </h2>
+      </motion.div>
+  </div>
     </motion.div>
     </div>
   );
